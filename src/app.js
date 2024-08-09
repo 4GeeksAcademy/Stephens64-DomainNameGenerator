17b5492134fd599cc358c8a01ed1cd9164b4ca64
stephens64 <96099094+stephens64@users.noreply.github.com>
2024-08-09 23:41:31 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,47 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ["the", "my", "your", "our", "that"];
+
+  let adjectives = [
+    "awesome",
+    "gorgeous",
+    "great",
+    "wonderful",
+    "unbelievable",
+    "amazing",
+    "weird",
+    "strange",
+    "crazy",
+    "funny",
+    "happy"
+  ];
+
+  let nouns = [
+    "unicorn",
+    "fox",
+    "geek",
+    "puppy",
+    "guitar",
+    "place",
+    "club",
+    "harp",
+    "house",
+    "cyborg",
+    "metalgear"
+  ];
+
+  let domains = [".net", ".com", ".org", ".edu", ".biznus", ".info"];
+
+  for (let pronoun of pronouns) {
+    for (let adjective of adjectives) {
+      for (let noun of nouns) {
+        for (let domain of domains) {
+          document.querySelector("#domainGenerator").innerHTML +=
+            "<h2>" + pronoun + adjective + noun + domain + "</h2>";
+        }
+      }
+    }
+  }
 };
